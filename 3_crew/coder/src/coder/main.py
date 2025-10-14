5fbfd109f5542baa8681bbc4c172d0b1b22fbcb8
Ramachandran Subramanian <rameasy@gmail.com>
2025-10-14 21:30:33 +0100

    Completed  coder project week 3 day 4

--- 3_crew/coder/src/coder/main.py
+++ 3_crew/coder/src/coder/main.py
@@ -11,7 +11,7 @@ warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 # Create output directory if it doesn't exist
 os.makedirs('output', exist_ok=True)
 
-assignment = 'Write a python program to calculate the first 10,000 terms \
+assignment = 'Write a python program to calculate the first 1,000 terms \
     of this series, multiplying the total by 4: 1 - 1/3 + 1/5 - 1/7 + ...'
 
 def run():
